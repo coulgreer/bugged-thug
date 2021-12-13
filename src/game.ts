f50c4fceefa48615c45bfa04ad89a2db79c93350
cagreer18 <coulgreer1@hotmail.com>
2021-12-13 11:11:17 -0500

    feat: Add card assets
    Implement the card images needed for the basic player and opponent
    decks. Also, includes the card backing.

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,25 @@
 import * as Phaser from 'phaser';
-import logo from './images/logo.png';
+import cardBackImage from './images/card-back.png';
+import chitChatImage from './images/chit-chat.png';
+import prodImage from './images/prod.png';
+import smallTalkImage from './images/small-talk.png';
+import suspectImage from './images/suspect.png';
+import thinkImage from './images/think.png';
+
+const cardBackName = 'card-back';
 
 class Scene extends Phaser.Scene {
   preload() {
-    this.load.image('logo', logo);
+    this.load.image(cardBackName, cardBackImage);
+    this.load.image('chit-chat', chitChatImage);
+    this.load.image('prod', prodImage);
+    this.load.image('small-talk', smallTalkImage);
+    this.load.image('suspect', suspectImage);
+    this.load.image('think', thinkImage);
   }
 
   create() {
-    this.add.image(200, 200, 'logo');
+    this.add.image(210, 280, cardBackName);
   }
 }
 
